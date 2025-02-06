7c385a75b5fc7d742cdb9b030c91a734a2241391
A. Developers <hinaarooj978@gmail.com>
2025-02-06 22:05:34 +0500

    fix: update RAPID_API_KEY in .env and switch baseUrl to localhost for local development

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,2 +1,2 @@
-export const baseUrl = "https://ssmprovider.onrender.com/api";
-// export const baseUrl = "http://localhost:4000/api";
+// export const baseUrl = "https://ssmprovider.onrender.com/api";
+export const baseUrl = "http://localhost:4000/api";
